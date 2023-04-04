9cef49f867dd7290470eb42421c80dae3af05f66
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2023-04-04 14:09:25 +0200

    Temporary update for index.html, index.js, and style.css

---
+++ js/index.js
@@ -0,0 +1,93 @@
+const baseUrl = "https://pricevaluation.azurewebsites.net/";
+// https://localhost:44376/
+
+Vue.createApp({
+  data() {
+    return {
+      homes: [],
+      id: 0,
+      municipalityId: 0,
+      squarePrice: "",
+      isOpen: null,
+      price: 0,
+      kvm: 0,
+      daysForSale: 0,
+      errormessage: "",
+      singleHome: null,
+      totalHomesForSale: "",
+      avgSquarePrice: "",
+    };
+  },
+
+  // async created() {
+  //     try {
+  //         const url = baseUrl + "municipality/totalhomesforsale" + "/" + this.municipalityId
+  //         const url2 = baseUrl + "municipality/avgkvmprice" + "/" + this.municipalityId
+  //         response = await axios.get(url)
+  //         response2 = await axios.get(url2)
+  //         this.totalHomesForSale = await response.data
+  //         this.avgSquarePrice = await response2.data
+  //     } catch(ex) {
+  //         alert(ex.message)
+  //     }
+  // },
+
+  // async created() {
+  //     try {
+  //         const url = baseUrl + "municipality/avgkvmprice" + "/" + this.municipalityId
+  //         response = await axios.get(url)
+  //         this.avgSquarePrice = await response.data
+  //     } catch(ex) {
+  //         alert(ex.message)
+  //     }
+  // },
+
+  methods: {
+    async helperGetAndShow(url) {
+      try {
+        const url = baseUrl + "api/prices";
+        const response = await axios.get(url);
+        this.homes = await response.data;
+      } catch (ex) {
+        alert(ex.message);
+      }
+    },
+
+    getAllHomes() {
+      this.helperGetAndShow(baseUrl);
+    },
+
+    async getById(id) {
+      const url = baseUrl + "api/prices" + "/" + id;
+      this.errormessage = "No such id";
+      try {
+        response = await axios.get(url);
+        this.singleHome = await response.data;
+      } catch {
+        alert(this.errormessage);
+      }
+    },
+
+    async getTotalHomesForSale(municipalityId) {
+      try {
+        const url =
+          baseUrl + "municipality/totalhomesforsale" + "/" + municipalityId;
+        response = await axios.get(url);
+        this.totalHomesForSale = await response.data;
+      } catch {
+        alert(this.errormessage);
+      }
+    },
+
+    async getAvgSquarePrice(municipalityId) {
+      const url = baseUrl + "municipality/avgkvmprice" + "/" + municipalityId;
+      this.errormessage = "So price data";
+      try {
+        response = await axios.get(url);
+        this.avgSquarePrice = await response.data;
+      } catch {
+        alert(this.errormessage);
+      }
+    },
+  },
+}).mount("#app");
